ab080b636ad67514e779ad622b11c58c77243878
Gary LePeep <amezheritskiy@gmail.com>
2022-04-18 00:23:02 -0400

    Added charts

--- weapon-data.js
+++ weapon-data.js
@@ -1,8 +1,15 @@
 var WEAPONS = [
     {
-        "Name": "PPSh-41"
+
     },
     {
-        "Name": "PPS-43"
+        "Name": "PPSh-41",
+        "Damage" : "75",
+        "RPM" : "324"
+    },
+    {
+        "Name": "PPS-43",
+        "Damage" : "34",
+        "RPM" : "768"
     }
 ];
