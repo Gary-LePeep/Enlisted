526415f53488b26470e77fdd5a01587fe85e3f78
Gary LePeep <amezheritskiy@gmail.com>
2022-05-14 19:41:44 -0400

    Added soldiers stat page

---
+++ soldier_stats/stats.js
@@ -0,0 +1,200 @@
+var SOLDIER_STATS = [
+    [
+        {
+            "Name" : "class_Default",
+            "Stats" : [4, 4, 4],
+            "Talent" : ""
+        }
+    ],
+    [
+        {
+            "Name" : "class_Rifleman#I",
+            "Stats" : [5, 5, 5],
+            "Talent" : "+10.5% speed of decreasing of the shot spread after quick turning of the firearm"
+        },
+        {
+            "Name" : "class_Rifleman#II",
+            "Stats" : [6, 6, 6],
+            "Talent" : "+5.3% sprint speed"
+        },
+        {
+            "Name" : "class_Rifleman#III",
+            "Stats" : [7, 7, 7],
+            "Talent" : "+15% medpack usage speed"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Medic",
+            "Stats" : [4, 6, 5],
+            "Talent" : "+3.5% run speed"
+        },
+        {
+            "Name" : "class_Assaulter#I",
+            "Stats" : [6, 4, 5],
+            "Talent" : "+35% climbing speed"
+        },
+        {
+            "Name" : "class_Assaulter#II",
+            "Stats" : [7, 5, 6],
+            "Talent" : "+35% stamina"
+        },
+        {
+            "Name" : "class_Assaulter#III",
+            "Stats" : [8, 6, 7],
+            "Talent" : "+100% health restored by medpack"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Engineer#I",
+            "Stats" : [5, 6, 4],
+            "Talent" : "+21% movement speed while crawling or crouching"
+        },
+        {
+            "Name" : "class_Engineer#II",
+            "Stats" : [6, 8, 4],
+            "Talent" : "+35% stamina regen speed"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Sniper#I",
+            "Stats" : [6, 2, 7],
+            "Talent" : "+14% maximum jump height"
+        },
+        {
+            "Name" : "class_Sniper#II",
+            "Stats" : [6, 2, 8],
+            "Talent" : "+35% breath holding time while aiming"
+        },
+        {
+            "Name" : "class_Sniper#III",
+            "Stats" : [6, 2, 10],
+            "Talent" : "+4.8% firearm reload speed"
+        }
+    ],
+    [
+        {
+            "Name" : "class_ATGunner#I",
+            "Stats" : [2, 6, 7],
+            "Talent" : "+52.5% speed of changing pose"
+        },
+        {
+            "Name" : "class_ATGunner#II",
+            "Stats" : [2, 8, 8],
+            "Talent" : "+17.5% throwing range of grenades"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Machinegunner#I",
+            "Stats" : [4, 4, 7],
+            "Talent" : "+3.5% run speed"
+        },
+        {
+            "Name" : "class_Machinegunner#II",
+            "Stats" : [4, 4, 10],
+            "Talent" : "+7% weapon aim speed"
+        },
+        {
+            "Name" : "class_Machinegunner#III",
+            "Stats" : [5, 6, 10],
+            "Talent" : "+100% health restored by medpack"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Radio#I",
+            "Stats" : [9, 4, 2],
+            "Talent" : "+17.5% firearms changing speed"
+        },
+        {
+            "Name" : "class_Radio#II",
+            "Stats" : [10, 6, 2],
+            "Talent" : "+70% health restored by medpack"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Mortarman#I",
+            "Stats" : [3, 6, 6],
+            "Talent" : " -17.5% effect of head-shaking caused by nearby explosions"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Flametrooper#I",
+            "Stats" : [6, 9, 3],
+            "Talent" : "+28% aim stability with firearms after receiving damage"
+        },
+        {
+            "Name" : "class_Flametrooper#II",
+            "Stats" : [6, 11, 4],
+            "Talent" : "+12.3% vitality"
+        }
+    ],
+    [
+        {
+            "Name" : "class_FighterPilot#I",
+            "Stats" : [4, 6, 5],
+            "Talent" : "+5% flight stamina regen speed"
+        },
+        {
+            "Name" : "class_FighterPilot#II",
+            "Stats" : [5, 7, 6],
+            "Talent" : "+5% flight stamina"
+        }
+    ],
+    [
+        {
+            "Name" : "class_AttackerPilot#I",
+            "Stats" : [4, 5, 6],
+            "Talent" : "-5% power of control loss after blackout"
+        },
+        {
+            "Name" : "class_AttackerPilot#II",
+            "Stats" : [5, 6, 7],
+            "Talent" : "+10% angle of focused and peripheral vision"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Tanker#I",
+            "Stats" : [7, 2, 6],
+            "Talent" : "+10% speed of changing seat in vehicle"
+        },
+        {
+            "Name" : "class_Tanker#II",
+            "Stats" : [9, 2, 7],
+            "Talent" : "+5% speed of vehicle repair"
+        },
+        {
+            "Name" : "class_Tanker#III",
+            "Stats" : [11, 2, 8],
+            "Talent" : "+45% speed of gear shift"
+        }
+    ],
+    [
+        {
+            "Name" : "class_Rider#I",
+            "Stats" : [6, 4, 8],
+            "Talent" : "+7% of vehicle repair"
+        }
+    ],
+]
+
+var SELL_STATS = [
+    {
+        "Name" : "class_rank#I",
+        "Sell" : ["2", "2", "2 (4)", "5 (10)", "28"]
+    },
+    {
+        "Name" : "class_rank#II",
+        "Sell" : ["3", "3", "5", "14", "54"]
+    },
+    {
+        "Name" : "class_rank#III",
+        "Sell" : ["3", "3", "5", "18", "82"]
+    }
+]
