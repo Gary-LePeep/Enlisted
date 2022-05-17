faaef8a788361c0c86a66e238d5b9d6e75d7ec3c
Gary LePeep <amezheritskiy@gmail.com>
2022-05-16 23:14:04 -0400

    fixed formatting for stats

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ recalibrateSize();
 window.addEventListener('resize', recalibrateSize);
 
 function recalibrateSize() {
-    document.getElementsByTagName('BODY')[0].style.transform = "scale(" + window.innerWidth / 1920 + "," + window.innerHeight / 927 + ")";
+    document.getElementsByTagName('BODY')[0].style.transform = "scale(" + window.innerWidth / 1920 + "," + window.innerWidth / 1920 + ")";
     document.getElementsByTagName('BODY')[0].style.transformOrigin = "0 0";
 }
 
