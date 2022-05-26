0e7c876b29e419f423a173d6820512d17196c8da
Gary LePeep <amezheritskiy@gmail.com>
2022-05-25 20:39:57 -0400

    Linted all code files

--- common.js
+++ common.js
@@ -1,61 +1,64 @@
+const BODY = document.getElementsByTagName('BODY')[0];
+
+function recalibrateSize() {
+  BODY.style.transform = `scale(${window.innerWidth / 1920},${window.innerWidth / 1920})`;
+  BODY.style.transformOrigin = '0 0';
+}
+
 recalibrateSize();
 window.addEventListener('resize', recalibrateSize);
 
-function recalibrateSize() {
-    document.getElementsByTagName('BODY')[0].style.transform = "scale(" + window.innerWidth / 1920 + "," + window.innerWidth / 1920 + ")";
-    document.getElementsByTagName('BODY')[0].style.transformOrigin = "0 0";
-}
-
 function initCSS() {
-    // Create new css link Element
-    var css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.type = 'text/css';
-    css.href = '../style.css';
-    document.getElementsByTagName('HEAD')[0].appendChild(css);
+  // Create new css link Element
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.type = 'text/css';
+  css.href = '../style.css';
+  document.getElementsByTagName('HEAD')[0].appendChild(css);
 }
 
 function titleBar(active) {
-    var topnav = document.createElement("div");
-    topnav.className = "topnav";
-    topnav.style = "position: absolute; top: 0px; left: 0px; width: 1700px; height: 53px";
-    document.getElementsByTagName('BODY')[0].appendChild(topnav);
-    var compareButton = document.createElement("button");
-    if (active == "comparison") {
-        compareButton.className = "active";
-    }
-    compareButton.textContent = translate("linkWeaponCompare");
-    compareButton.onclick = function () { window.location = "https://gary-lepeep.github.io/Enlisted/comparison" };
-    topnav.appendChild(compareButton);
-    var datamineButton = document.createElement("button");
-    if (active == "datamine") {
-        datamineButton.className = "active";
-    }
-    datamineButton.textContent = translate("linkDatamine");
-    datamineButton.onclick = function () { window.location = "https://gary-lepeep.github.io/Enlisted/datamine" };
-    topnav.appendChild(datamineButton);
-    var updateButton = document.createElement("button");
-    if (active == "updates") {
-        updateButton.className = "active";
-    }
-    updateButton.textContent = translate("linkUpdate");
-    updateButton.onclick = function () { window.location = "https://gary-lepeep.github.io/Enlisted/updates" };
-    topnav.appendChild(updateButton);
-    // warning if update was recent
-    if (wasRecent(parseInt(document.cookie))) {
-        updateButton.className = "blink-bg";
-        updateButton.textContent += "⚠️";
-    }
-    var statsButton = document.createElement("button");
-    if (active == "statistics") {
-        statsButton.className = "active";
-    }
-    statsButton.textContent = translate("linkStats");
-    statsButton.onclick = function () { window.location = "https://gary-lepeep.github.io/Enlisted/soldier_stats" };
-    topnav.appendChild(statsButton);
+  const topnav = document.createElement('div');
+  topnav.className = 'topnav';
+  topnav.style = 'position: absolute; top: 0px; left: 0px; width: 1700px; height: 53px';
+  BODY.appendChild(topnav);
+  const compareButton = document.createElement('button');
+  if (active === 'comparison') {
+    compareButton.className = 'active';
+  }
+  compareButton.textContent = translate('linkWeaponCompare');
+  compareButton.onclick = () => { window.location = 'https://gary-lepeep.github.io/Enlisted/comparison'; };
+  topnav.appendChild(compareButton);
+  const datamineButton = document.createElement('button');
+  if (active === 'datamine') {
+    datamineButton.className = 'active';
+  }
+  datamineButton.textContent = translate('linkDatamine');
+  datamineButton.onclick = () => { window.location = 'https://gary-lepeep.github.io/Enlisted/datamine'; };
+  topnav.appendChild(datamineButton);
+  const updateButton = document.createElement('button');
+  if (active === 'updates') {
+    updateButton.className = 'active';
+  }
+  updateButton.textContent = translate('linkUpdate');
+  updateButton.onclick = () => { window.location = 'https://gary-lepeep.github.io/Enlisted/updates'; };
+  topnav.appendChild(updateButton);
+  // warning if update was recent
+  if (wasRecent(parseInt(document.cookie))) {
+    updateButton.className = 'blink-bg';
+    updateButton.textContent += '⚠️';
+  }
+  const statsButton = document.createElement('button');
+  if (active === 'statistics') {
+    statsButton.className = 'active';
+  }
+  statsButton.textContent = translate('linkStats');
+  statsButton.onclick = () => { window.location = 'https://gary-lepeep.github.io/Enlisted/soldier_stats'; };
+  topnav.appendChild(statsButton);
 
-    topnav.appendChild(translate("language", "b", "position:absolute; top:20px; left:1700px; font-size:16px; text-align:center; color: white"));
-    var title = translate(active, "div");
-    title.className = "TITLE";
-    document.getElementsByTagName('BODY')[0].appendChild(title);
+  topnav.appendChild(translate(''
+  + 'language', 'b', 'position:absolute; top:20px; left:1700px;font-size:16px; text-align:center; color: white'));
+  const title = translate(active, 'div');
+  title.className = 'TITLE';
+  BODY.appendChild(title);
 }
--- updates/updates.js
+++ updates/updates.js
@@ -1,17 +1,17 @@
-var UPDATES = {
-    1651955031871: "This is the first entry. There are no recent weapons updates.",
-    1652293836395: [
-        "Added gun pps_42_red, which is a reskin of the pps_42 with no extra stats",
-        "Smoke grenades start after 2s instead of 3, and fade 6x slower",
-        "Grenade launchers reload and put on/take off 0.5-1s slower across the board",
-        "Grenade launcher grenade damage reduced from 50 to 20"
-    ],
-}
+const UPDATES = {
+  1651955031871: 'This is the first entry. There are no recent weapons updates.',
+  1652293836395: [
+    'Added gun pps_42_red, which is a reskin of the pps_42 with no extra stats',
+    'Smoke grenades start after 2s instead of 3, and fade 6x slower',
+    'Grenade launchers reload and put on/take off 0.5-1s slower across the board',
+    'Grenade launcher grenade damage reduced from 50 to 20',
+  ],
+};
 
 function numToDate(num) {
-    return new Date(parseInt(num)).toLocaleDateString()
+  return new Date(parseInt(num)).toLocaleDateString();
 }
 
 function wasRecent(num) {
-    return num < Object.keys(UPDATES)[Object.keys(UPDATES).length - 1];
+  return num < Object.keys(UPDATES)[Object.keys(UPDATES).length - 1];
 }
